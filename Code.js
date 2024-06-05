7e1468076ab217c7d6e8aa4bdcec9ae3a12c316c
Maccabee Levine <msl321@lehigh.edu>
2024-06-05 12:23:58 -0400

    Load retention, faculty author, circ counts and oclc number from FOLIO

--- Code.js
+++ Code.js
@@ -5,8 +5,22 @@ const TITLE = 'title';
 const CONTRIBUTOR = 'contributor';
 const PUBLICATION_DATE = 'publication_date';
 const ITEM_STATUS = 'item_status';
+const RETENTION = 'EAST Retention';
+const FACULTY_AUTHOR = 'Faculty Author';
+const LEGACY_CIRC_COUNT = 'ole_circ_count';
+const FOLIO_CIRC_COUNT = 'folio_circ_count';
+const OCLC_NUMBER = 'oclc_number';
 
 const MAX_COLUMNS = 100;
+
+// statistical codes
+const RETENTION_IDS = [
+  'ba16cd17-fb83-4a14-ab40-23c7ffa5ccb5',
+]
+
+const FACULTY_AUTHOR_NOTE_TEXT = "Lehigh Faculty Author Publication";
+const LEGACY_CIRC_COUNT_NOTE_TYPE_ID = '8f26b475-d7e3-4577-8bd0-c3d3bf44f73b';
+const OCLC_NUMBER_IDENTIFIER_TYPE_ID = '439bfbae-75bc-4f74-9fc7-b2a2d47ce3ef';
 
 function testEdit() {
   onEdit({
@@ -37,7 +51,11 @@ function barcodeChanged(row) {
   console.log("barcode changed in row " + row);
   const barcode = SpreadsheetApp.getActiveSheet().getRange(row, getColumn(BARCODE), 1, 1).getValue();
   const item = loadItem(barcode);
+  if (!item) {
+    console.error("No item matching barcode: " + barcode);
+  }
   item.instance = loadInstance(barcode);
+  item.circulations = loadCirculationLogs(item, 'Checked out');
   writeItemToSheet(row, item);
 }
 
@@ -73,20 +91,87 @@ function loadItem(barcode) {
 function loadInstance(barcode) {
   const config = JSON.parse(PropertiesService.getScriptProperties().getProperty("config"));
 
-  // execute query
-  const DQ = encodeURIComponent("\"");
+  // execute initial query -- to fine the instance ID
   const instancesQuery = FOLIOAUTHLIBRARY.getBaseOkapi(config.environment) +
     `/search/instances?query=${encodeURIComponent(`items.barcode=="${barcode}"`)}`;
   console.log('Loading instances with query: ', encodeURI(instancesQuery));
   const getOptions = FOLIOAUTHLIBRARY.getHttpGetOptions();
   const instancesResponse = UrlFetchApp.fetch(instancesQuery, getOptions);
 
-  // parse response
+  // parse initial response
   const instancesResponseText = instancesResponse.getContentText();
   const instances = JSON.parse(instancesResponseText)['instances'];
   let instance = instances[0] ?? null;
 
+  // must load the full instance for the full body, including notes
+  let instanceQuery = FOLIOAUTHLIBRARY.getBaseOkapi(config.environment) +
+    `/inventory/instances/${instance.id}`;
+  console.log('Loading full instance with query: ', encodeURI(instanceQuery));
+  const instanceResponse = UrlFetchApp.fetch(instanceQuery, getOptions);
+
+  // parse second response
+  const instanceResponseText = instanceResponse.getContentText();
+  instance = JSON.parse(instanceResponseText);
+
   return instance;
+}
+
+function loadCirculationLogs(item, action) {
+  const config = JSON.parse(PropertiesService.getScriptProperties().getProperty("config"));
+
+  // execute query
+  const logsQuery = FOLIOAUTHLIBRARY.getBaseOkapi(config.environment) +
+    `/audit-data/circulation/logs?query=${encodeURIComponent(`(items=="*${item.id}*" and action=="${action}")`)}`;
+  console.log('Loading circ logs with query: ', encodeURI(logsQuery));
+  const getOptions = FOLIOAUTHLIBRARY.getHttpGetOptions();
+  const logsResponse = UrlFetchApp.fetch(logsQuery, getOptions);
+
+  // parse response
+  const logsResponseText = logsResponse.getContentText();
+  const logs = JSON.parse(logsResponseText);
+
+  return logs;
+}
+
+function hasRetentionAgreement(item) {
+  const statisticalCodeIds = item['statisticalCodeIds'];
+  const retentionIds = statisticalCodeIds.filter(element => RETENTION_IDS.includes(element));
+  return retentionIds.length > 0;
+}
+
+function isFacultyAuthor(item) {
+  const notes = item.instance['notes'];
+  for (let note of notes) {
+    if (FACULTY_AUTHOR_NOTE_TEXT == note['note']) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function parseLegacyCircCount(item) {
+  const notes = item['notes'];
+  for (let note of notes) {
+    if (note.itemNoteTypeId == LEGACY_CIRC_COUNT_NOTE_TYPE_ID) {
+      return note.note;
+    }
+  }
+  return null;
+}
+
+function parseFolioCircCount(item) {
+  const circ_count = item.circulations['totalRecords'];
+  return circ_count;
+}
+
+function parseOclcNumber(item) {
+  const identifiers = item.instance['identifiers'];
+  for (let identifier of identifiers) {
+    if (OCLC_NUMBER_IDENTIFIER_TYPE_ID == identifier['identifierTypeId']) {
+      return identifier['value'];
+    }
+  }
+  return null;
 }
 
 function writeItemToSheet(row, item) {
@@ -95,6 +180,11 @@ function writeItemToSheet(row, item) {
   writeToSheet(row, getColumn(CONTRIBUTOR), item['contributorNames']?.[0]?.['name'] ?? '');
   writeToSheet(row, getColumn(PUBLICATION_DATE), item.instance['publication']?.[0]?.['dateOfPublication']);
   writeToSheet(row, getColumn(ITEM_STATUS), item['status']['name']);
+  writeToSheet(row, getColumn(RETENTION), hasRetentionAgreement(item));
+  writeToSheet(row, getColumn(FACULTY_AUTHOR), isFacultyAuthor(item));
+  writeToSheet(row, getColumn(LEGACY_CIRC_COUNT), parseLegacyCircCount(item));
+  writeToSheet(row, getColumn(FOLIO_CIRC_COUNT), parseFolioCircCount(item));
+  writeToSheet(row, getColumn(OCLC_NUMBER), parseOclcNumber(item));
 }
 
 function writeToSheet(row, column, value) {
